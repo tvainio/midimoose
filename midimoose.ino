7b41b0944748a3222dba7fe8d379b5dec3c921bd
Teemu Vainio <tvainiotheprogrammer@gmail.com>
2019-07-08 22:36:25 +0300

    divider settings are now stored in EEPROM

--- midimoose.ino
+++ midimoose.ino
@@ -1,8 +1,8 @@
 // Maps midi channels 15 to 1 and 16 to 2. Filter out other channels. Filters
 // out everything except note on, note off and mod wheel.
 
+#include <EEPROM.h>
 #include <MIDI.h>
-#include <SoftwareSerial.h>
 #include "clock_divider.h" 
 #include "clock_output.h"
 
@@ -12,23 +12,26 @@
 #define PPQ 96
 
 // weird hardware, weird pin mapping...
-#define OUT1 9
-#define OUT2 10
-#define OUT3 8
-#define OUT4 13
-#define OUT5 12
-#define OUT6 11
+#define OUT1_PIN 9
+#define OUT2_PIN 10
+#define OUT3_PIN 8
+#define OUT4_PIN 13
+#define OUT5_PIN 12
+#define OUT6_PIN 11
+
+uint8_t readDividerFromEEPROM(uint8_t output) {
+	uint8_t value = EEPROM.read(output);
+	if (value > THIRTYTWO) return SIXTEEN;
+}
 
 ClockOutput output[] = {
-	ClockOutput(FULL, OUT1),
-	ClockOutput(TWO, OUT2),
-	ClockOutput(FOUR, OUT3),
-	ClockOutput(EIGHT, OUT4),
-	ClockOutput(SIXTEEN, OUT5),
-	ClockOutput(THIRTYTWO, OUT6),
+	ClockOutput(readDividerFromEEPROM(0), OUT1_PIN),
+	ClockOutput(readDividerFromEEPROM(1), OUT2_PIN),
+	ClockOutput(readDividerFromEEPROM(2), OUT3_PIN),
+	ClockOutput(readDividerFromEEPROM(3), OUT4_PIN),
+	ClockOutput(readDividerFromEEPROM(4), OUT5_PIN),
+	ClockOutput(readDividerFromEEPROM(5), OUT6_PIN),
 };
-
-//SoftwareSerial debugSerial(10, 11); // RX, TX
 
 void updateOutputs(uint8_t divider, bool state) {
 	for (uint8_t i=0;i<NUMBER_OF_OUTPUTS; i++) {
@@ -69,7 +72,15 @@ ClockDivider full(PPQ*4, cbFull);
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 uint8_t mapCCToDivider(uint8_t cc) {
-	return cc/22; // roughly split the 0-127 range to 0-5
+	return cc/22; // roughly split the 0-127 midi CC range to 0-5 divider
+}
+
+void updateDivider(uint8_t outputNumber, uint8_t ccValue) {
+	uint8_t value = mapCCToDivider(ccValue);
+	if (value > THIRTYTWO)
+		return;
+	output[outputNumber].setDivider(value);
+	EEPROM.update(outputNumber, value);
 }
 
 void handleControlChange(byte channel, byte number, byte value) {
@@ -80,22 +91,22 @@ void handleControlChange(byte channel, byte number, byte value) {
 		}
 		// CC 20-26 control midi moose clock outputs
     else if (number == CLOCK_OUT_CC_FIRST) {
-			output[0].setDivider(mapCCToDivider(value));
+			updateDivider(0, value);
 		}
     else if (number == CLOCK_OUT_CC_FIRST+1) {
-			output[1].setDivider(mapCCToDivider(value));
+			updateDivider(1, value);
 		}
     else if (number == CLOCK_OUT_CC_FIRST+2) {
-			output[2].setDivider(mapCCToDivider(value));
+			updateDivider(2, value);
 		}
     else if (number == CLOCK_OUT_CC_FIRST+3) {
-			output[3].setDivider(mapCCToDivider(value));
+			updateDivider(3, value);
 		}
     else if (number == CLOCK_OUT_CC_FIRST+4) {
-			output[4].setDivider(mapCCToDivider(value));
+			updateDivider(4, value);
 		}
     else if (number == CLOCK_OUT_CC_FIRST+5) {
-			output[5].setDivider(mapCCToDivider(value));
+			updateDivider(5, value);
 		}
   }
 }
@@ -105,14 +116,11 @@ void handleNoteOn(byte channel, byte pitch, byte velocity)
   if (channel > 14)
   {
     MIDI.sendNoteOn(pitch, velocity, channel - 14);
-    //         debugSerial.print(channel, HEX);
   }
 }
 
 void handleNoteOff(byte channel, byte pitch, byte velocity)
 {
-  //    debugSerial.println("off");
-
   if (channel > 14) {
     MIDI.sendNoteOff(pitch, velocity, channel - 14);
   }
@@ -129,17 +137,12 @@ void handleClock(void) {
 
 void setup()
 {
-  //    debugSerial.begin(115200);
-  //    debugSerial.println("Hello, world?");
-
   MIDI.setHandleClock(handleClock);
   MIDI.setHandleNoteOn(handleNoteOn);
   MIDI.setHandleNoteOff(handleNoteOff);
   MIDI.setHandleControlChange(handleControlChange);
   MIDI.begin(MIDI_CHANNEL_OMNI);
-
   MIDI.turnThruOff();
-
 }
 
 void loop()
