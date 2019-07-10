0a1b73fc062cfc9a7525438b374e56a37d12ea81
Teemu Vainio <tvainiotheprogrammer@gmail.com>
2019-07-10 22:08:57 +0300

    midi start now resets counters. Whitespace fixes.

--- midimoose.ino
+++ midimoose.ino
@@ -20,46 +20,46 @@
 #define OUT6_PIN 11
 
 uint8_t readDividerFromEEPROM(uint8_t output) {
-	uint8_t value = EEPROM.read(output);
-	if (value > THIRTYTWO) return SIXTEEN;
+  uint8_t value = EEPROM.read(output);
+  if (value > THIRTYTWO) return SIXTEEN;
 }
 
 ClockOutput output[] = {
-	ClockOutput(readDividerFromEEPROM(0), OUT1_PIN),
-	ClockOutput(readDividerFromEEPROM(1), OUT2_PIN),
-	ClockOutput(readDividerFromEEPROM(2), OUT3_PIN),
-	ClockOutput(readDividerFromEEPROM(3), OUT4_PIN),
-	ClockOutput(readDividerFromEEPROM(4), OUT5_PIN),
-	ClockOutput(readDividerFromEEPROM(5), OUT6_PIN),
+  ClockOutput(readDividerFromEEPROM(0), OUT1_PIN),
+  ClockOutput(readDividerFromEEPROM(1), OUT2_PIN),
+  ClockOutput(readDividerFromEEPROM(2), OUT3_PIN),
+  ClockOutput(readDividerFromEEPROM(3), OUT4_PIN),
+  ClockOutput(readDividerFromEEPROM(4), OUT5_PIN),
+  ClockOutput(readDividerFromEEPROM(5), OUT6_PIN),
 };
 
 void updateOutputs(uint8_t divider, bool state) {
-	for (uint8_t i=0;i<NUMBER_OF_OUTPUTS; i++) {
-		output[i].setState(divider, state);
-	}
+  for (uint8_t i=0;i<NUMBER_OF_OUTPUTS; i++) {
+    output[i].setState(divider, state);
+  }
 }
 
 void cbThirtytwo(bool state) {
-	updateOutputs(THIRTYTWO, state);
+  updateOutputs(THIRTYTWO, state);
 }
 void cbSixteen(bool state) {
-	updateOutputs(SIXTEEN, state);
+  updateOutputs(SIXTEEN, state);
 }
 
 void cbEight(bool state) {
-	updateOutputs(EIGHT, state);
+  updateOutputs(EIGHT, state);
 }
 
 void cbFour(bool state) {
-	updateOutputs(FOUR, state);
+  updateOutputs(FOUR, state);
 }
 
 void cbTwo(bool state) {
-	updateOutputs(TWO, state);
+  updateOutputs(TWO, state);
 }
 
 void cbFull(bool state) {
-	updateOutputs(FULL, state);
+  updateOutputs(FULL, state);
 }
 
 ClockDivider thirtytwo(PPQ/8, cbThirtytwo);
@@ -72,42 +72,42 @@ ClockDivider full(PPQ*4, cbFull);
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 uint8_t mapCCToDivider(uint8_t cc) {
-	return cc/22; // roughly split the 0-127 midi CC range to 0-5 divider
+  return cc/22; // roughly split the 0-127 midi CC range to 0-5 divider
 }
 
 void updateDivider(uint8_t outputNumber, uint8_t ccValue) {
-	uint8_t value = mapCCToDivider(ccValue);
-	if (value > THIRTYTWO)
-		return;
-	output[outputNumber].setDivider(value);
-	EEPROM.update(outputNumber, value);
+  uint8_t value = mapCCToDivider(ccValue);
+  if (value > THIRTYTWO)
+    return;
+  output[outputNumber].setDivider(value);
+  EEPROM.update(outputNumber, value);
 }
 
 void handleControlChange(byte channel, byte number, byte value) {
   if (channel > 14) 
   {
-		if(number == 0x01) { // Only send mod wheel to acid moose
-			MIDI.sendControlChange(number, value, channel - 14);
-		}
-		// CC 20-26 control midi moose clock outputs
+    if(number == 0x01) { // Only send mod wheel to acid moose
+      MIDI.sendControlChange(number, value, channel - 14);
+    }
+    // CC 20-26 control midi moose clock outputs
     else if (number == CLOCK_OUT_CC_FIRST) {
-			updateDivider(0, value);
-		}
+      updateDivider(0, value);
+    }
     else if (number == CLOCK_OUT_CC_FIRST+1) {
-			updateDivider(1, value);
-		}
+      updateDivider(1, value);
+    }
     else if (number == CLOCK_OUT_CC_FIRST+2) {
-			updateDivider(2, value);
-		}
+      updateDivider(2, value);
+    }
     else if (number == CLOCK_OUT_CC_FIRST+3) {
-			updateDivider(3, value);
-		}
+      updateDivider(3, value);
+    }
     else if (number == CLOCK_OUT_CC_FIRST+4) {
-			updateDivider(4, value);
-		}
+      updateDivider(4, value);
+    }
     else if (number == CLOCK_OUT_CC_FIRST+5) {
-			updateDivider(5, value);
-		}
+      updateDivider(5, value);
+    }
   }
 }
 
@@ -135,8 +135,18 @@ void handleClock(void) {
  full.tick();
 }
 
+void handleStart(void) {
+ thirtytwo.reset();
+ sixteen.reset();
+ eight.reset();
+ fourth.reset();
+ two.reset();
+ full.reset();
+}
+
 void setup()
 {
+  MIDI.setHandleStart(handleStart);
   MIDI.setHandleClock(handleClock);
   MIDI.setHandleNoteOn(handleNoteOn);
   MIDI.setHandleNoteOff(handleNoteOff);
